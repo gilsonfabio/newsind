d9d9cbf89c291aa29fc3f7508aa7626966733b71
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2026-03-27 16:21:32 -0300

    Atualização da versão do next

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,3 +4,9 @@ export const api = axios.create({
     baseURL: "http://localhost:3333"
     //baseURL: "https://backdelivery.vercel.app/" 
 })
+
+/*
+.env
+NEXTAUTH_SECRET=fc72ba03-b91a-4277-8218-b6ceef990734
+NEXTAUTH_URL=http://localhost:3000
+*/
