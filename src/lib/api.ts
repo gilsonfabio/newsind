4bda80a11b2f513b256b4f2aa417e5da02f9ca4a
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2026-03-30 11:29:37 -0300

    Nova versão next16 mudança api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://newsind.vercel.app:3333"
+    baseURL: "https://backcaldascard.vercel.app/"
     //baseURL: "https://backdelivery.vercel.app/" 
 })
 
