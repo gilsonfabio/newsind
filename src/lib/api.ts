aaac01332c002567f3b78087dac21b04b3052151
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2026-03-30 14:09:35 -0300

    Alteração no endereço da api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,8 +5,3 @@ export const api = axios.create({
     //baseURL: "https://backdelivery.vercel.app/" 
 })
 
-/*
-.env
-NEXTAUTH_SECRET=fc72ba03-b91a-4277-8218-b6ceef990734
-NEXTAUTH_URL=http://localhost:3000
-*/
