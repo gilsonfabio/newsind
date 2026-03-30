d23ad4e72d485a58acc9ca75f519f53f59859f81
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2026-03-30 11:21:54 -0300

    Atualização next16 shadcn-ui

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "http://newsind.vercel.app:3333"
     //baseURL: "https://backdelivery.vercel.app/" 
 })
 
