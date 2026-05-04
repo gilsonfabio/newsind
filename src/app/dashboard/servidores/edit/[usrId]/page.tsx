28d8f7431d53c00d5cdd72b6f8869327817cee54
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2026-05-04 16:08:21 -0300

    Atualização da data de admissão do servidor

--- src/app/dashboard/servidores/edit/[usrId]/page.tsx
+++ src/app/dashboard/servidores/edit/[usrId]/page.tsx
@@ -154,16 +154,11 @@ export default function EditServidorPage() {
     api.get(`searchUser/${id}`).then((res) => {
       const u = res.data[0];
 
-      const formatDate = (date: string) => {
-        if (!date) return "";
-
-        const d = new Date(date);
-        const year = d.getUTCFullYear();
-        const month = String(d.getUTCMonth() + 1).padStart(2, "0");
-        const day = String(d.getUTCDate()).padStart(2, "0");
-
-        return `${year}-${month}-${day}`;
-      };
+      function formatDate(dateString: string) {
+        const date = new Date(dateString);
+        date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
+        return date.toISOString().split("T")[0];
+      }
 
       setForm((prev) => ({
         ...prev,
